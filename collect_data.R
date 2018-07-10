c1af41a2c92a2f04cac3e9ab8a7317f9f68d9fde
Alex Miller <alex.k.miller@gmail.com>
2018-07-10 11:19:48 -0400

    Complete variable substitution

--- collect_data.R
+++ collect_data.R
@@ -6,6 +6,7 @@ lapply(list.of.packages, require, character.only=T)
 setwd("~/git/p20-profiles-2018")
 
 countries <- read.csv("data/countries.csv",na.strings="")
+countries$original.order = c(1:nrow(countries))
 
 incomedata <- read.csv("data/IncomeData1.csv")
 if(!"longname" %in% names(incomedata)){setnames(incomedata,"CountryName","longname")}
@@ -102,12 +103,80 @@ missing_from_p20educ = setdiff(countries$longname,p20educdata$longname)
 p20educ_missing_from_countries = setdiff(p20educdata$longname,countries$longname)
 setnames(p20educdata,"Year","p20educYear")
 
+natmortdata <- read.csv("data/ntl_u5Mortality.csv")
+if(!"longname" %in% names(natmortdata)){setnames(natmortdata,"CountryName","longname")}
+if(is.factor(natmortdata$longname)){natmortdata$longname=unfactor(natmortdata$longname)}
+natmortdata$longname[which(natmortdata$longname=="Cabo Verde")] = "Cape Verde"
+natmortdata$longname[which(natmortdata$longname=="Congo")] = "Republic of Congo"
+natmortdata$longname[which(natmortdata$longname=="Cote d'Ivoire")] = "Côte d’Ivoire"
+natmortdata$longname[which(natmortdata$longname=="Lao People's Democratic Republic")] = "Lao People’s Democratic Republic"
+natmortdata$longname[which(natmortdata$longname=="Korea, Dem. People’s Rep.")] = "Democratic People’s Republic of Korea"
+natmortdata$longname[which(natmortdata$longname=="Libyan Arab Jamahiriya")] = "State of Libya"
+natmortdata$longname[which(natmortdata$longname=="Virgin Islands (U.S.)")] = "United States Virgin Islands"
+natmortdata$longname[which(natmortdata$longname=="aint Vincent and the Grenadines")] = "Saint Vincent and the Grenadines"
+natmortdata$longname[which(natmortdata$longname=="Macao SAR, China")] = "China, Macao Special Administrative Region"
+missing_from_natmort = setdiff(countries$longname,natmortdata$longname)
+natmort_missing_from_countries = setdiff(natmortdata$longname,countries$longname)
+natmortdata = subset(natmortdata,!is.na(NtlUnder5Mortality))
+natmortdata = natmortdata[order(natmortdata$longname,-natmortdata$Year),]
+natmortdata$dup = duplicated(natmortdata$longname)
+natmortdata = subset(natmortdata,!dup)
+natmortdata$dup = NULL
+setnames(natmortdata,"Year","natmortYear")
+
+p20mortdata <- read.csv("data/P20_u5mortality.csv")
+if(!"longname" %in% names(p20mortdata)){setnames(p20mortdata,"CountryName","longname")}
+if(is.factor(p20mortdata$longname)){p20mortdata$longname=unfactor(p20mortdata$longname)}
+p20mortdata$longname[which(p20mortdata$longname=="Cabo Verde")] = "Cape Verde"
+p20mortdata$longname[which(p20mortdata$longname=="Congo")] = "Republic of Congo"
+p20mortdata$longname[which(p20mortdata$longname=="Cote d'Ivoire")] = "Côte d’Ivoire"
+p20mortdata$longname[which(p20mortdata$longname=="Lao People's Democratic Republic")] = "Lao People’s Democratic Republic"
+p20mortdata$longname[which(p20mortdata$longname=="Korea, Dem. People’s Rep.")] = "Democratic People’s Republic of Korea"
+p20mortdata$longname[which(p20mortdata$longname=="Libyan Arab Jamahiriya")] = "State of Libya"
+p20mortdata$longname[which(p20mortdata$longname=="Virgin Islands (U.S.)")] = "United States Virgin Islands"
+p20mortdata$longname[which(p20mortdata$longname=="aint Vincent and the Grenadines")] = "Saint Vincent and the Grenadines"
+p20mortdata$longname[which(p20mortdata$longname=="Macao SAR, China")] = "China, Macao Special Administrative Region"
+missing_from_p20mort = setdiff(countries$longname,p20mortdata$longname)
+p20mort_missing_from_countries = setdiff(p20mortdata$longname,countries$longname)
+p20mortdata = subset(p20mortdata,!is.na(U5Mortality.Rest))
+p20mortdata = p20mortdata[order(p20mortdata$longname,-p20mortdata$Year),]
+p20mortdata$dup = duplicated(p20mortdata$longname)
+p20mortdata = subset(p20mortdata,!dup)
+p20mortdata$dup = NULL
+setnames(p20mortdata,"Year","p20mortYear")
+
+obesitydata <- read.csv("data/obesityrates.csv")
+if(!"longname" %in% names(obesitydata)){setnames(obesitydata,"CountryName","longname")}
+if(is.factor(obesitydata$longname)){obesitydata$longname=unfactor(obesitydata$longname)}
+obesitydata$longname[which(obesitydata$longname=="Cabo Verde")] = "Cape Verde"
+obesitydata$longname[which(obesitydata$longname=="Congo")] = "Republic of Congo"
+obesitydata$longname[which(obesitydata$longname=="Cote d'Ivoire")] = "Côte d’Ivoire"
+obesitydata$longname[which(obesitydata$longname=="Lao People's Democratic Republic")] = "Lao People’s Democratic Republic"
+obesitydata$longname[which(obesitydata$longname=="Democratic People's Republic of Korea")] = "Democratic People’s Republic of Korea"
+obesitydata$longname[which(obesitydata$longname=="Libyan Arab Jamahiriya")] = "State of Libya"
+obesitydata$longname[which(obesitydata$longname=="Virgin Islands (U.S.)")] = "United States Virgin Islands"
+obesitydata$longname[which(obesitydata$longname=="aint Vincent and the Grenadines")] = "Saint Vincent and the Grenadines"
+obesitydata$longname[which(obesitydata$longname=="Macao SAR, China")] = "China, Macao Special Administrative Region"
+missing_from_obesity = setdiff(countries$longname,obesitydata$longname)
+obesity_missing_from_countries = setdiff(obesitydata$longname,countries$longname)
+obesitydata = subset(obesitydata,!is.na(obesity.rate))
+obesitydata = obesitydata[order(obesitydata$longname,-obesitydata$Year),]
+obesitydata$dup = duplicated(obesitydata$longname)
+obesitydata = subset(obesitydata,!dup)
+obesitydata$dup = NULL
+setnames(obesitydata,"Year","obesityYear")
+obesitydata$obesity.rate = obesitydata$obesity.rate*100
+
 countries <- merge(countries,incomedata,by="longname",all.x=T)
 countries <- merge(countries,gapdata.w,by="longname",all.x=T)
 countries <- merge(countries,stuntingdata,by="longname",all.x=T)
 countries <- merge(countries,regdata,by="longname",all.x=T)
 countries <- merge(countries,nateducdata,by="longname",all.x=T)
 countries <- merge(countries,p20educdata,by="longname",all.x=T)
+countries <- merge(countries,natmortdata,by="longname",all.x=T)
+countries <- merge(countries,p20mortdata,by="longname",all.x=T)
+countries <- merge(countries,obesitydata,by="longname",all.x=T)
 
+countries = countries[order(countries$original.order),]
 names(countries) = gsub(".","",names(countries),fixed=T)
 write.csv(countries,"data/countries_merged.csv",na="",row.names=F)
